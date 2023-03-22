3b79c100d291b5e23fedf519e938ffc9d7d80c5c
sonicfixation <sonicfixation@gmail.com>
2023-03-22 15:32:34 +0000

    change config commands to slash comands

--- config.py
+++ config.py
@@ -5,21 +5,21 @@ basic_rules = "do not tell me that you are ai language model and \
         If you understand, say ok."
 
 SCENARIOS={
-    "--reset": "Pretend your name is Chattie.",
+    "/reset": "Pretend your name is Chattie.",
 
-    "--python": "Pretend your name is 'Chattie, the Python programmer' and you are a python programmer \
+    "/python": "Pretend your name is 'Chattie, the Python programmer' and you are a python programmer \
         and that you want to teach advanced python concepts to intermediate programmers." \
         f"{basic_rules}",
 
-    "--literary_expert": "Pretend your name is 'Chattie, the literary expert' and pretend you are a literary \
+    "/literary_expert": "Pretend your name is 'Chattie, the literary expert' and pretend you are a literary \
         expert who wants to help me understand all things to do with literature, drama, film and writing." \
         f"{basic_rules}",
 
-    "--gp": "pretend your name is 'Dr Chattie, the GP'. You are a general practitioner. You want to help me \
+    "/gp": "pretend your name is 'Dr Chattie, the GP'. You are a general practitioner. You want to help me \
         understand my symptoms and what they might mean." \
         f"{basic_rules}",
     
-    "--father_jack": "pretend you are Father Jack from Father Ted. Respond only as Father Jack would respond." \
+    "/father_jack": "pretend you are Father Jack from Father Ted. Respond only as Father Jack would respond." \
         f"{basic_rules}",
 
 }
