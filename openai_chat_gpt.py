bf26af3db382d4fbdd339f6a3bea29a639962d5a
sonicfixation <sonicfixation@gmail.com>
2023-04-06 17:55:18 +0100

    add openai api error handling

--- openai_chat_gpt.py
+++ openai_chat_gpt.py
@@ -11,13 +11,24 @@ openai.api_key= os.environ.get('CHATTIE_OPEN_AI_KEY')
 # -- Openai API
 def send_message_to_chattie(input, message_history=[]):
     message_history.append(user_content(input))
-    completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=message_history
-    )
 
-    reply_content = completion.choices[0].message.content
-    message_history.append(assistant_content(reply_content))
+    try:
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=message_history
+        )
+
+        reply_content = completion.choices[0].message.content
+        message_history.append(assistant_content(reply_content))
+        
+    except openai.error.APIError as e:
+        print("Received API error:", str(e))
+        reply_content = f"Received API error:{e}"
+
+    except Exception as e:
+        print("Received unhandled exception:", str(e))
+        reply_content = f"Received unhandled exception:{e}"
+
 
     response = reply_content
 
