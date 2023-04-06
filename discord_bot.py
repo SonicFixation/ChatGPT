c71b453f54e884845eca9af0731f07fea867e36a
sonicfixation <sonicfixation@gmail.com>
2023-04-06 19:14:55 +0100

    chunk large responses

--- discord_bot.py
+++ discord_bot.py
@@ -1,7 +1,7 @@
 import discord
 import os
+import textwrap
 from openai_chat_gpt import send_message_to_chattie
-
 from payload_creation import (
     create_message_history_list,
     get_scenario
@@ -42,7 +42,14 @@ async def on_message(message):
     else:
         response, message_history = send_message_to_chattie(message.content, message_history)
 
-    await message.channel.send(response)
+    if len(response) > 2000:
+        # Break long responses into smaller chinks so Discord won't complain
+        response_chunks = textwrap.wrap(response, width=2000, break_long_words=False)
+        for chunk in response_chunks:
+            await message.channel.send(chunk)
+
+    else:
+        await message.channel.send(response)
 
 # -- Run the bot
 client.run(discord_bot_token)
